d83e2f9a979873b4c78b3a78203f902e3d7890a6
Uriel Jimenez <ujimenez@griddynamics.com>
2022-06-24 12:25:35 -0500

    Initial commit

---
+++ src/test/java/com/example/booksStorage/BooksStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
