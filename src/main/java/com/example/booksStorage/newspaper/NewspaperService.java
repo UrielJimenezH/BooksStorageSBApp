fcc994b21e93d0d94cb536007b6d26ae380d6c31
Uriel Jimenez <ujimenez@griddynamics.com>
2022-08-29 11:59:27 -0500

    NewspaperService, NewspaperRepository and tests created

--- src/main/java/com/example/booksStorage/newspaper/NewspaperService.java
+++ src/main/java/com/example/booksStorage/newspaper/NewspaperService.java
@@ -4,70 +4,42 @@ import com.example.booksStorage.Item;
 import com.example.booksStorage.exceptionshandling.NoSuchElementFoundException;
 import com.example.booksStorage.observer.EventManager;
 import com.example.booksStorage.observer.EventManagerConfig;
-import com.example.booksStorage.repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class NewspaperService {
-    private final Repository<Long, Item> repository;
-    private final EventManager<Item> eventManager;
-
     @Autowired
-    public NewspaperService(
-            @Qualifier("treeMapRepository") Repository<Long, Item> repository,
-            EventManager<Item> eventManager
-    ) {
-        this.repository = repository;
-        this.eventManager = eventManager;
-    }
+    private NewspaperRepository repository;
+    @Autowired
+    private EventManager<Item> eventManager;
 
     public List<Newspaper> getAll() {
-        return repository.getAll()
-                .stream()
-                .filter(item -> item instanceof Newspaper)
-                .map(item -> (Newspaper) item)
-                .collect(Collectors.toList());
+        return repository.getAll();
     }
 
-    public Newspaper get(Long newspaperId) {
-        return repository.get(newspaperId)
-                .filter(item -> item instanceof Newspaper)
-                .map(item -> (Newspaper) item)
-                .orElseThrow(() -> new NoSuchElementFoundException("Newspaper with id " + newspaperId + " does not exist"));
+    public Newspaper get(Long id) {
+        return repository.get(id)
+                .orElseThrow(() -> new NoSuchElementFoundException("Newspaper with id " + id + " does not exist"));
     }
 
     public Newspaper add(Newspaper newspaper) {
-        repository.save(newspaper.getId(), newspaper);
-        eventManager.notifySubscribers(EventManagerConfig.NEWSPAPER_CREATION_EVENT, newspaper);
-        return newspaper;
+        Newspaper newNewspaper = repository.save(newspaper);
+        eventManager.notifySubscribers(EventManagerConfig.BOOK_CREATION_EVENT, newspaper);
+        return newNewspaper;
     }
 
-    public Newspaper update(Long newspaperId, Newspaper newNewspaper) {
-        return repository.get(newspaperId)
-                .filter(item -> item instanceof Newspaper)
-                .map(item -> {
-                    Newspaper newspaper = (Newspaper) item;
-                    newNewspaper.setId(newspaperId);
-                    newNewspaper.setRegistrationDate(newspaper.getRegistrationDate());
-                    repository.update(newspaperId, newNewspaper);
-                    return newNewspaper;
-                })
-                .orElseThrow(() -> new NoSuchElementFoundException("Newspaper with id " + newspaperId + " does not exist"));
+    public Newspaper update(Long id, Newspaper newNewspaper) {
+        newNewspaper.setId(id);
+        Optional<Newspaper> updatedNewspaper = repository.update(newNewspaper);
+        return updatedNewspaper
+                .orElseThrow(() -> new NoSuchElementFoundException("Newspaper with id " + id + " does not exist"));
     }
 
-    public Newspaper delete(Long newspaperId) {
-        return repository.get(newspaperId)
-                .filter(item -> item instanceof Newspaper)
-                .map(item -> {
-                    Newspaper newspaper = (Newspaper) item;
-                    repository.delete(newspaperId);
-                    return newspaper;
-                })
-                .orElseThrow(() -> new NoSuchElementFoundException("Newspaper with id " + newspaperId + " does not exist"));
+    public Newspaper delete(Long id) {
+        return repository.delete(id)
+                .orElseThrow(() -> new NoSuchElementFoundException("Newspaper with id " + id + " does not exist"));
     }
 }
---
+++ src/test/java/com/example/booksStorage/NewspaperRepositoryTest.java
@@ -0,0 +1,128 @@
+package com.example.booksStorage;
+
+import com.example.booksStorage.newspaper.Newspaper;
+import com.example.booksStorage.newspaper.NewspaperRepository;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class NewspaperRepositoryTest {
+    private static NewspaperRepository repository;
+    private final Newspaper newspaper = new Newspaper("Summary", 3, LocalDate.now(), "Technology Newspaper", "Publisher");
+
+    @BeforeClass
+    public static void init() {
+        EmbeddedDatabase db = new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("schema.sql")
+                .addScript("newspapers-test-data.sql")
+                .build();
+
+        repository = new NewspaperRepository();
+        ReflectionTestUtils.setField(repository, "jdbcTemplate", new JdbcTemplate(db));
+    }
+
+    @Test
+    public void get_ReturnsNewspaper_WhenTableHasRegistersAlready() {
+        Optional<Newspaper> newspaper = repository.get(1L);
+        assertTrue(newspaper.isPresent());
+        assertEquals("Newspaper 1", newspaper.get().getTitle());
+    }
+
+    @Test
+    public void get_ReturnsEmptyNewspaper_WhenNewspaperWithGivenIdDoesNotExists() {
+        Optional<Newspaper> newspaper = repository.get(32L);
+        assertTrue(newspaper.isEmpty());
+    }
+
+    @Test
+    public void get_ReturnsEmptyNewspaper_WhenNewspaperWithGivenIdIsNull() {
+        Optional<Newspaper> newspaper = repository.get(null);
+        assertTrue(newspaper.isEmpty());
+    }
+
+    @Test
+    public void getAll_ReturnsNonEmptyList() {
+        List<Newspaper> newspapers = repository.getAll();
+        assertFalse(newspapers.isEmpty());
+    }
+
+    @Test
+    public void save_ReturnsNewspaper() {
+        Newspaper savedNewspaper = repository.save(newspaper);
+
+        assertNotNull(savedNewspaper.getId());
+        assertEquals(newspaper.getTitle(), savedNewspaper.getTitle());
+    }
+
+    @Test
+    public void get_ReturnsNewspaper_WhenPreviouslyInserted() {
+        Newspaper savedNewspaper = repository.save(newspaper);
+
+        Optional<Newspaper> newspaper2 = repository.get(savedNewspaper.getId());
+        newspaper2.ifPresentOrElse((b) -> {
+            assertEquals(newspaper.getTitle(), b.getTitle());
+        }, () -> {
+            throw new RuntimeException("Not found");
+        });
+    }
+
+    @Test
+    public void update_ReturnsEmptyNewspaper_WhenNewspaperWithGivenIdDoesNotExists() {
+        newspaper.setId(50L);
+        Optional<Newspaper> savedNewspaper = repository.update(newspaper);
+
+        assertFalse(savedNewspaper.isPresent());
+    }
+
+    @Test
+    public void update_ReturnsEmptyNewspaper_WhenNewspaperWithGivenIdIsNull() {
+        Optional<Newspaper> savedNewspaper = repository.update(newspaper);
+
+        assertFalse(savedNewspaper.isPresent());
+    }
+
+    @Test
+    public void update_ReturnsNewspaper_WhenNewspaperWithGivenIdIsExists() {
+        newspaper.setId(5L);
+        Optional<Newspaper> savedNewspaper = repository.update(newspaper);
+
+        savedNewspaper.ifPresentOrElse((b) -> {
+            assertEquals(newspaper.getTitle(), b.getTitle());
+        }, () -> {
+            throw new RuntimeException("Not found");
+        });
+    }
+
+
+    @Test
+    public void delete_ReturnsEmptyNewspaper_WhenNewspaperWithGivenIdDoesNotExists() {
+        Optional<Newspaper> newspaper = repository.delete(50L);
+
+        assertFalse(newspaper.isPresent());
+    }
+
+    @Test
+    public void delete_ReturnsNewspaper_WhenNewspaperWithGivenIdIsExists() {
+        Optional<Newspaper> newspaper = repository.delete(6L);
+
+        newspaper.ifPresentOrElse((b) -> {
+            assertNotNull(b.getTitle());
+        }, () -> {
+            throw new RuntimeException("Not found");
+        });
+    }
+}
