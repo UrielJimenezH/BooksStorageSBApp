b6ed4d159155b650ba7f07549b21a174540862fe
Uriel Jimenez <ujimenez@griddynamics.com>
2022-08-29 09:35:51 -0500

    LetterService, LetterRepository and tests created

---
+++ src/test/resources/letters-test-data.sql
@@ -0,0 +1,99 @@
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 1',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 2',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 3',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 4',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 5',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 6',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 7',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 8',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 9',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
+
+INSERT INTO Letters VALUES(
+    NULL,
+    'Person 10',
+    'Summary',
+    3,
+    '1998-04-23',
+    '2020-04-20',
+    NULL
+);
