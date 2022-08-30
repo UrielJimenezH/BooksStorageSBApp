d601fe3a6dfd8eb471423fbdf7f681ec4fe959c5
Uriel Jimenez <ujimenez@griddynamics.com>
2022-08-30 12:51:19 -0500

    Config parameters moved to properties file

--- src/main/java/com/example/booksStorage/DataSourceConfiguration.java
+++ src/main/java/com/example/booksStorage/DataSourceConfiguration.java
@@ -1,26 +1,32 @@
 package com.example.booksStorage;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DataSourceConfiguration {
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(
+            @Value("${jdbc.url}") String url,
+            @Value("${jdbc.username}") String username,
+            @Value("${jdbc.password}") String password
+    ) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost/BooksStorage");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
         return dataSource;
     }
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource());
+        return new JdbcTemplate(dataSource);
     }
 }
